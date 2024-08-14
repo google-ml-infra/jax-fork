f79f69dcb19d798b87588962af76935447792463
Michael Hudgins <michaelhudgins@google.com>
2024-08-14 07:12:46 +0000

    Modify wait for connection to be a reusable composite action

--- actions/ci_connection/wait_for_connection.py
+++ actions/ci_connection/wait_for_connection.py
@@ -1,3 +1,17 @@
+# Copyright 2024 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 from multiprocessing.connection import Listener
 import time
@@ -25,7 +39,7 @@ def wait_for_notification(address):
                         break
                     print("Received message")
                     if message == "keep_alive":
-                        print("keep alive received")
+                        print("Keep alive received")
                         last_time = time.time()
                         continue  # Keep-alive received, continue waiting
                     elif message == "closed":
