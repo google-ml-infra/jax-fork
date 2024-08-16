189a019578d7f70b82b6fc9b0caf68826732b8ab
Michael Hudgins <michaelhudgins@google.com>
2024-08-16 13:27:00 +0000

    Parameterize the workflow

--- actions/ci_connection/notify_connection.py
+++ actions/ci_connection/notify_connection.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import signal
 import time
 import threading
 import subprocess
@@ -22,31 +20,32 @@ from multiprocessing.connection import Client
 lock = threading.Lock()
 
 # Configuration (same as wait_for_connection.py)
-address = ('localhost', 12455)
+address = ("localhost", 12455)
 keep_alive_interval = 30  # 30 seconds
 
+
 def timer(conn):
-    while True:
-        # We lock because closed and keep_alive could technically arrive at the same time
-        with lock:
-            conn.send("keep_alive")
-        time.sleep(keep_alive_interval)
-       
+  while True:
+    # We lock because closed and keep_alive could technically arrive at the same time
+    with lock:
+      conn.send("keep_alive")
+    time.sleep(keep_alive_interval)
+
 
 if __name__ == "__main__":
-    with Client(address) as conn:
-        conn.send("connected")
+  with Client(address) as conn:
+    conn.send("connected")
 
-        # Thread is running as a daemon so it will quit when the
-        # main thread terminates.
-        timer_thread = threading.Thread(target=timer, daemon = True,args=(conn,))
-        timer_thread.start()
+    # Thread is running as a daemon so it will quit when the
+    # main thread terminates.
+    timer_thread = threading.Thread(target=timer, daemon=True, args=(conn,))
+    timer_thread.start()
 
-        print("Entering interactive bash session")
-        # Enter interactive bash session
-        subprocess.run(['/bin/bash', '-i'])
+    print("Entering interactive bash session")
+    # Enter interactive bash session
+    subprocess.run(["/bin/bash", "-i"])
 
-        print("Exiting interactive bash session")
-        with lock:
-            conn.send("closed")
-        conn.close()
+    print("Exiting interactive bash session")
+    with lock:
+      conn.send("closed")
+    conn.close()
--- actions/ci_connection/wait_for_connection.py
+++ actions/ci_connection/wait_for_connection.py
@@ -20,87 +20,91 @@ import threading
 import sys
 
 last_time = time.time()
-timeout = 600 # 10 minutes for initial connection
-keep_alive_timeout=900 # 30 minutes for keep-alive if no closed message (allow for reconnects)
+timeout = 600  # 10 minutes for initial connection
+keep_alive_timeout = (
+  900  # 30 minutes for keep-alive if no closed message (allow for reconnects)
+)
 
 
 def wait_for_notification(address):
-    """Waits for connection notification from the listener."""
-    global last_time
-    while True:
-        with Listener(address) as listener:
-            print("Waiting for connection")
-            with listener.accept() as conn:
-                while True:
-                    try:
-                        message = conn.recv()
-                    except EOFError as e: 
-                        print("EOFError occurred:", e)
-                        break
-                    print("Received message")
-                    if message == "keep_alive":
-                        print("Keep alive received")
-                        last_time = time.time()
-                        continue  # Keep-alive received, continue waiting
-                    elif message == "closed":
-                        print("Connection closed by the other process.")
-                        return  # Graceful exit
-                    elif message == "connected":
-                        last_time = time.time()
-                        timeout = keep_alive_timeout
-                        print("Connected")
-                    else:
-                        print("Unknown message received:", message)
-                        continue
+  """Waits for connection notification from the listener."""
+  global last_time
+  while True:
+    with Listener(address) as listener:
+      print("Waiting for connection")
+      with listener.accept() as conn:
+        while True:
+          try:
+            message = conn.recv()
+          except EOFError as e:
+            print("EOFError occurred:", e)
+            break
+          print("Received message")
+          if message == "keep_alive":
+            print("Keep alive received")
+            last_time = time.time()
+            continue  # Keep-alive received, continue waiting
+          elif message == "closed":
+            print("Connection closed by the other process.")
+            return  # Graceful exit
+          elif message == "connected":
+            last_time = time.time()
+            timeout = keep_alive_timeout
+            print("Connected")
+          else:
+            print("Unknown message received:", message)
+            continue
+
 
 def timer():
-    while True:
-        print("Checking status")
-        time_elapsed = time.time() - last_time
-        if time_elapsed < timeout:
-            print(f"Time since last keepalive {int(time_elapsed)}s")
-        else:
-            print("Timeout reached, exiting")
-            os.kill(os.getpid(), signal.SIGTERM)
-        time.sleep(60)
+  while True:
+    print("Checking status")
+    time_elapsed = time.time() - last_time
+    if time_elapsed < timeout:
+      print(f"Time since last keepalive {int(time_elapsed)}s")
+    else:
+      print("Timeout reached, exiting")
+      os.kill(os.getpid(), signal.SIGTERM)
+    time.sleep(60)
+
 
 if __name__ == "__main__":
-    address = ('localhost', 12455)  # Address and port to listen on
-    # Check if we should wait for the connection
-    wait_for_connection = False
-    if os.environ.get("WAIT_ON_ERROR") == "1": 
-        print("WAIT_ON_ERROR is set")
-        if os.getppid() != 1:
-            print("Previous command did not exit with success, waiting for connection")
-            wait_for_connection = True
-        else:
-            print("Previous command exited with success")
-    else: 
-        print("WAIT_ON_ERROR is not set")
-
-    if os.environ.get("INTERACTIVE_CI") == "1":
-        print("INTERACTIVE_CI is set, waiting for connection")
-        wait_for_connection = True
+  address = ("localhost", 12455)  # Address and port to listen on
+  # Check if we should wait for the connection
+  wait_for_connection = False
+  if os.environ.get("WAIT_ON_ERROR") == "1":
+    print("WAIT_ON_ERROR is set")
+    if os.getppid() != 1:
+      print("Previous command did not exit with success, waiting for connection")
+      wait_for_connection = True
     else:
-        print("INTERACTIVE_CI is not set")
+      print("Previous command exited with success")
+  else:
+    print("WAIT_ON_ERROR is not set")
 
-    if not wait_for_connection:
-        print("No condition was met to wait for connection. Continuing Job")
-        exit(0)
+  if os.environ.get("INTERACTIVE_CI") == "1":
+    print("INTERACTIVE_CI is set, waiting for connection")
+    wait_for_connection = True
+  else:
+    print("INTERACTIVE_CI is not set")
 
-    # Grab and print the data required to connect to this vm
-    host = os.environ.get("HOSTNAME")
-    repo = os.environ.get("REPOSITORY")
+  if not wait_for_connection:
+    print("No condition was met to wait for connection. Continuing Job")
+    exit(0)
 
-    print(f"Connection parameters: '{host} {repo}'")
+  # Grab and print the data required to connect to this vm
+  host = os.environ.get("HOSTNAME")
+  repo = os.environ.get("REPOSITORY")
 
-    # Thread is running as a daemon so it will quit when the
-    # main thread terminates.
-    timer_thread = threading.Thread(target=timer, daemon = True)
-    timer_thread.start()
+  print(f"Connection parameters: '{host} {repo}'")
 
-    wait_for_notification(address)  # Wait for connection and get the connection object
+  # Thread is running as a daemon so it will quit when the
+  # main thread terminates.
+  timer_thread = threading.Thread(target=timer, daemon=True)
+  timer_thread.start()
 
-    print("Exiting connection wait loop.")
-    # Force a flush so we don't miss messages
-    sys.stdout.flush()
+  wait_for_notification(address)  # Wait for connection and get the connection object
+
+  print("Exiting connection wait loop.")
+  # Force a flush so we don't miss messages
+  sys.stdout.flush()
