6500c18b1d3ed60e9b528959b6914daa249792c8
Vladimir Belitskiy <belitskiy@google.com>
2024-09-19 17:45:34 +0000

    Don't fail the job on wait-for-connection step failure.

--- actions/ci_connection/get_labels.py
+++ actions/ci_connection/get_labels.py
@@ -1,3 +1,17 @@
+# Copyright 2024 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Retrieve PR labels, if any.
 
 While these labels are also available via GH context, and the event payload
@@ -24,7 +38,11 @@ def retrieve_labels(print_to_stdout: bool = True) -> list[str]:
   In case this is not a PR, return an empty list.
   """
   # Check if this is a PR (pull request)
-  github_ref = os.getenv('GITHUB_REF')
+  github_ref = os.getenv('GITHUB_REF', '')
+  if not github_ref:
+    raise TypeError('GITHUB_REF is not defined. '
+                    'Is this being run outside of GitHub Actions?')
+
   # Outside a PR context - no labels to be found
   if not github_ref.startswith('refs/pull/'):
     logging.debug('Not a PR workflow run, returning an empty label list')
